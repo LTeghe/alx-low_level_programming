5aa7eb12abf5372c3a3b658bcd8db81fbf86cd53
LTeghe <lennyteghe@gmail.com>
2022-07-01 14:12:41 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
-#!/bin/bash
-#iclude <stdio.h>
+ #! / bin / bash
+#iclude < stdio.h >
 
 /**
- *main - print the string in the put function
+ *main - Entry point
+ *print the string in the put function
  *
  *Description: using the main function
  *this program prints "Programming is like building a multilingual puzzle"
- *Return :0
+ *Return :Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programinig is like building a multilingual puzzle");
 	return (0);
-
 }
