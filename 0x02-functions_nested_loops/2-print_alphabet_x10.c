059dfe9cdd55d9c2b326bceaeb2735f1ca6440e0
LTeghe <lennyteghe@gmail.com>
2022-07-05 14:31:15 -0700

    Print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10x - print alphabet
+ *
+ * Return: Always 0.
+ */
+
+
+void print_alphabet_10x(void)
+{
+	int alphabet;
+	int count;
+
+	count - 0;
+	while (count < 10)
+	{
+		for (alphabet = 'a';
+				alphabet < = 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+
+		count++;
+		_putchar('\n');
+	}
+
+}
