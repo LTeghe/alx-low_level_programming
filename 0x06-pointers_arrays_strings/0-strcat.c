cc51df318be576fcdaeb8611ebf2debf8b70a8d8
LTeghe <lennyteghe@gmail.com>
2022-07-13 11:50:59 -0700

    Strc

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,15 +15,14 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 
-	j=0;
+	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		j++
-			i++;
+		j++;
+		i++;
 	}
 
 	dest[i] = '\0';
-	
 	return (dest);
 }
