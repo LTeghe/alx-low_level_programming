c18acab873d5c76b215ddc85ba967fc77d551d33
LTeghe <lennyteghe@gmail.com>
2022-07-18 13:48:41 -0700

    function that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,13 +2,13 @@
 
 /**
  **_strchr - main function.
- * @s: Pointer to the string.
- * @c: Character to locate in the string.
+ *@s: Pointer to the string.
+ *@c: Character to locate in the string.
  *
- * Description: This function locates a character in a string.
+ *Description: This function locates a character in a string.
  *
- * Return: a pointwer to the first occurance of the character c 
- * in the string s, or NULL if the character is not found.
+ *Return: a pointwer to the first occurance of the character c
+ *in the string s, or NULL if the character is not found.
  */
 
 char *_strchr(char *s, char c)
