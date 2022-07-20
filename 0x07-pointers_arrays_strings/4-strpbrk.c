b1ee46ea4673e3b215b3a7ea8bae4a78180b5fc5
LTeghe <lennyteghe@gmail.com>
2022-07-20 10:44:33 -0700

    searches a string of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,18 +12,26 @@ i#include "main.h"
  */
 
 char *_strpbrk(char *s, char *accept)
+
 {
 	int i;
+
 	while (*s)
+
 	{
 		for (i = 0; accept[i]; i++)
+
 		{
 			if (accept[i] == *s)
+
 			{
 				return (s);
 			}
+
 		}
+
 		s++;
 	}
-	return (NULL);
+	
+	return (NULL)
 }
